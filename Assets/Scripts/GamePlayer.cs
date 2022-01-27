5522edbfe395b163d00c3790acfa4c9c41c9ed02
Iris <irisoostra@upcmail.nl>
2022-01-27 14:43:03 +0100

    Added gestures and working on state controller and state fading

--- Assets/Scripts/GamePlayer.cs
+++ Assets/Scripts/GamePlayer.cs
@@ -6,18 +6,67 @@ public class GamePlayer : MonoBehaviour {
     [SerializeField]
     private List<GameState> gameStates = null;
 
+    [SerializeField]
+    private float minTimeBetweenStates = 10.0f;
+
+    [SerializeField]
+    private Animator fadeAnimator;
+
     private int currentState;
+    private int endState;
+
+    private float timer = 0.0f;
+    private int zero = 0;
+    private int timerInSeconds = 0;
+    private int amountOfMSInASecond = 60;
+
+    private bool isCounting = false;
+
     // Start is called before the first frame update
     void Start() {
-        currentState = 0;
-    }
+        currentState = 0; //Maybe hierboven zetten
+        if(gameStates.Count > 0) {
+            endState = gameStates.Count;
+        } else {
+            endState = 0;
+        }
 
-    //maybe private
-    public void NextState() {
+        StartTimer();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if(isCounting) {
+            TimerCheck();
+        }
+    }
+
+    //maybe private
+    public void NextState() {
+        if(!isCounting) {
+            currentState++;
+            fadeAnimator.SetTrigger("Fadeout");
+            Debug.Log("Current State: " + currentState);
+            StartTimer();
+        }
+    }
+
+    private void StartTimer() {
+        isCounting = true;
+    }
+
+    private void ResetTimer() {
+        isCounting = false;
+        timer = zero;
+        timerInSeconds = zero;
+    }
+
+    public void TimerCheck() {
+        timer += Time.deltaTime;
+        timerInSeconds = (int) timer % amountOfMSInASecond;
+
+        if(timerInSeconds >= minTimeBetweenStates) {
+            ResetTimer();
+        }
     }
 }
